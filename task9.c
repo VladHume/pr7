90f91143520e123acd1609cfac26d5a64d600dc3
VladHume <152102439+VladHume@users.noreply.github.com>
2025-06-19 12:30:25 +0300

    Create task9.c

---
+++ task9.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/time.h>
+
+int main(void) {
+    struct timeval t0, t1;
+    gettimeofday(&t0, NULL);
+    for (volatile int i = 0; i < 10000000; i++);
+    
+    gettimeofday(&t1, NULL);
+    long ms = (t1.tv_sec - t0.tv_sec) * 1000 +
+              (t1.tv_usec - t0.tv_usec) / 1000;
+    printf("Час: %ld мс\n", ms);
+    return 0;
+}
