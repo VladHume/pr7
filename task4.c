bffcb859d9246943e34ab58334c60f5a7e9e63e2
VladHume <152102439+VladHume@users.noreply.github.com>
2025-06-19 12:28:38 +0300

    Create task4.c

---
+++ task4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s файл [файл2 ...]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        FILE *f = fopen(argv[i], "r");
+        if (!f) { perror(argv[i]); continue; }
+
+        char buf[512];
+        int count = 0;
+        while (fgets(buf, sizeof(buf), f)) {
+            fputs(buf, stdout);
+            if (++count == 20) {
+                printf("-- Більше? (натисніть Enter)");
+                getchar();
+                count = 0;
+            }
+        }
+
+        fclose(f);
+    }
+
+    return EXIT_SUCCESS;
+}
